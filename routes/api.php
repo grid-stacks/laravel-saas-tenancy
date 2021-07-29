43a7df02542b6f1dde33f0ae703b5c2cbebea371
Sajib Hasan <sajib001@gmail.com>
2021-07-30 00:29:58 +0600

    feat(auth): :sparkles: Implement passport auth
    Client secret install: php artisan passport:install

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('test', function (Request $request) {
+    return "Not authorized route";
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
