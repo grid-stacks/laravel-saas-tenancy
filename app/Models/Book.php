5467ec828fd248f3373c53215f8b1d9631d82021
Sajib Hasan <sajib001@gmail.com>
2021-07-31 00:35:43 +0600

    feat(graphql): :sparkles: Implement basic graphql operations

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'description', 'publication_year'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
