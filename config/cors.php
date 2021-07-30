bf79d53c8ec9a5264fa6b9da392be19e4ef7b1b5
Sajib Hasan <sajib001@gmail.com>
2021-07-31 01:01:58 +0600

    refactor(graphql): :wrench: Config graphql and cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'graphql-playground', 'graphql', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
